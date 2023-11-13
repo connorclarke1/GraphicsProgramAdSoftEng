d35d8d5131041d2dfd3810d4e0a09f7eb96417e3
Connor Clarke <connorclarke1@yahoo.com>
2023-11-13 06:44:04 +0000

    Added DrawTo Documentation

--- GraphicsProgram/DrawTo.cs
+++ GraphicsProgram/DrawTo.cs
@@ -7,8 +7,20 @@ using System.Threading.Tasks;
 
 namespace GraphicsProgram
 {
+    
     public class DrawTo
     {
+        /// <summary>
+        /// Draws a line onto GraphicsHandler.Graphics using GraphicsHandler pointer, colour and param radius
+        /// <br/>Example:<br/> Call this method to draw a line onto the bitmap image
+        ///     <code>
+        ///     DrawTo.Draw(50, 50);
+        ///     </code>
+        /// This will draw a line from the pointer position to endX, endY in the colour set in the graphicsHandler
+        /// </summary>
+        /// <param name="endX">X position to draw to</param>
+        /// <param name="endY">Y position to draw to</param>
+        /// <returns>void</returns>
         public static void Draw(GraphicsHandler graphicsHandler, int endX, int endY)
         {
             Pointer pointer = graphicsHandler.pointer;   
